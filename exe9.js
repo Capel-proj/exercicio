92f71f035c6bb460bd337c17967269f0b45dd342
capel-proj <capelluisfelipe2002@gmail.com>
2020-08-24 22:15:21 -0300

    Exercicio 9 realizado

--- exe9.js
+++ exe9.js
@@ -1,1 +1,8 @@
- 
+    function exe9 (numero){
+        for(var i = numero; i> numero ; i--{
+            if (numero % i == 0) {
+            soma = soma + i
+            }
+        }
+        console.log(`Soma dos divisores é ${soma}`)
+    }
